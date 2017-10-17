47c078442f251e027d94e4e7b983e6b54f8ddaac
Guillaume Favelier <guillaume.favelier@gmail.com>
2017-10-17 17:39:08 +0200

    Add a notification to the locking process and a README.md file

--- .script/lock.sh
+++ .script/lock.sh
@@ -4,6 +4,7 @@ src_img=/tmp/screenshot.png
 blur_img=/tmp/screenshot_blur.png
 final_img=/tmp/screenshot_final.png
 
+notify-send 'Locking the screen...'
 scrot $src_img
 convert $src_img -blur 0x5 $blur_img
 convert $blur_img ~/dotfiles/.data/lock.png -gravity center -composite -matte $final_img
---
+++ README.md
@@ -0,0 +1,6 @@
+# Dependencies
+In order to use these configuration files, you must install some dependencies. For archlinux distribution: `i3-wm dmenu i3status feh compton dunst libnotify scort imagemagick`
+
+# ToDo
++ automatic installation script
+
