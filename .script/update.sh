e93e08515329de3ce1cab4266ab8d8602bf5ed90
GuillaumeFavelier <GuillaumeFavelier@gmail.com>
2018-03-16 19:29:47 +0100

    Update package-checking system and notification system

--- .script/update.sh
+++ .script/update.sh
@@ -3,7 +3,7 @@
 # make display available
 export DISPLAY=:0
 
-notify-send 'checking source packages...'
+notify-send -u normal 'System' 'Checking source packages...'
 
 # upgrade environment
 source $HOME/.zshrc
@@ -22,14 +22,16 @@ packageList=''
 
 # remove all fancy options on ls
 alias ls=ls
-for i in $(ls $SRC_PATH $args)
+ls_cmd='ls '$SRC_PATH' '$args
+for i in $(eval $ls_cmd)
 do
   current=$SRC_PATH/$i
+  echo $current
 
   # move to the target directory
   cd $current
 
-  if [ ! -d .git ]; then
+  if [ ! -d '.git' ]; then
     if [ -d 'source' ]; then
       current=$current/source
       cd $current
@@ -41,31 +43,43 @@ do
 
   # update tracking refs
   ret=$(git remote update)
-  if [ $? -ne 0 ]
+  if [ $? -ne '0' ]
   then
     echo 'Error: problem during refs tracking.'
     continue
   fi
 
-  res=$(git rev-parse --abbrev-ref HEAD)
-  if [ $res != 'master' ]
+  ret=$(git rev-parse --abbrev-ref HEAD)
+  if [ $ret != 'master' ]
   then
     echo 'Error: not on master.'
     continue
   fi
 
   # check if updates available
-  res=$(git status -uno)
+  ret=$(git status -uno)
 
-  if [[ $res = *"behind"* ]] && [[ $res = *"fast-forward"* ]]
+  if [[ $ret = *"behind"* ]] && [[ $ret = *"fast-forward"* ]]
   then
     packageList+=$i' '
     echo $current >> $AUTO_UPDATE_FILE
+  elif [ -f 'PKGBUILD' ]
+  then
+    pkgname=$(makepkg --printsrcinfo | grep 'pkgname =' | cut -f 2 -d =)
+
+    # AUR: updated but check if installed
+    ret=$(pacman -Q ${pkgname/ /})
+    if [ $? -ne '0' ]
+    then
+      packageList+=$i' '
+      echo $current >> $AUTO_UPDATE_FILE
+    fi
+    echo $ret
   fi
 done
 
 toNotify=$(wc -l $AUTO_UPDATE_FILE | cut -f1 -d \  )
-if [ $toNotify -gt 0 ]
+if [ $toNotify -gt '0' ]
 then
-  notify-send $packageList
+  notify-send -u normal 'System' $packageList
 fi
